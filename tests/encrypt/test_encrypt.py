aed9ef21f49cb50fdc491b49eb5908931a64a54c
Leonardo Nunes <leonardonunes.uberbraga@gmail.com>
2023-12-01 23:10:58 +0000

    feat: requisito 2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,15 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message("hello", 1) == "h_olle"
+    assert encrypt_message("hello", 2) == "oll_eh"
+    assert encrypt_message("hello", 3) == "leh_ol"
+    assert encrypt_message("hello", 4) == "o_lleh"
+    assert encrypt_message("hello", 0) == "olleh"
+
+    with pytest.raises(TypeError):
+        encrypt_message("hello", -1) == "hello"
+        encrypt_message("hello", "5") == "hello"
+        encrypt_message(5, "hello") == "hello"
