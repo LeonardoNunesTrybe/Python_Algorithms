018b7374a496a198e3d05f293f61e34fb342303e
Leonardo Nunes <leonardonunes.uberbraga@gmail.com>
2023-12-01 22:49:06 +0000

    feat: requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    try:
+        students = 0
+
+        for start, end in permanence_period:
+            if start <= target_time <= end:
+                students += 1
+
+        return students
+
+    except TypeError:
+        return None
